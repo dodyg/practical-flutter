f4ccd120bc5bc9a62dc2648aa602d2bc86bdf3f2
Dody Gunawinata <dodyg@silverkeytech.com>
2018-01-28 17:32:23 +0200

    Initial hello world

---
+++ projects/ui/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+var x = new Center(
+      child: new Text(
+        'What I  do now',
+        textDirection: TextDirection.ltr,
+      ),
+    );
+
+void main() {
+  runApp(
+    x
+  );
+}
