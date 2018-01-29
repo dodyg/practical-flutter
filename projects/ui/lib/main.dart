5387c8de95ab30d31cccbd4efe350dec5b74ff2b
Dody Gunawinata <dodyg@silverkeytech.com>
2018-01-29 05:01:29 +0200

    Learn on how to show a message

--- projects/ui/lib/main.dart
+++ projects/ui/lib/main.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 
-var x = new Center(
-      child: new Text(
-        'What I  do now',
-        textDirection: TextDirection.ltr,
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+      child: new RaisedButton(
+        child: new Text(
+        "Good morning Tarakan",
       ),
+        onPressed: (){
+          var dlg = new AlertDialog(
+            title: new Text("Greeting"),
+            content: new Text("Hello World"),
+          );
+          showDialog(context:context, child: dlg);
+        },
+      )
+    ),
+    ) ;
+  }
+}
+
+var x = new MaterialApp(
+      home: new Home(),
     );
 
 void main() {
   runApp(
     x
   );
+}
+
+void onPressed() {
 }
