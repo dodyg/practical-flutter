c1f836a16371dff0f92ed054f41eac5f250290d2
Dody Gunawinata <dodyg@silverkeytech.com>
2018-02-14 04:36:58 +0200

    Add drawer functionality

--- projects/ui/lib/pages/scaffold_page.dart
+++ projects/ui/lib/pages/scaffold_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-class ScaffoldPage extends StatelessWidget{
+class ScaffoldPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -9,16 +8,21 @@ class ScaffoldPage extends StatelessWidget{
         title: new Text("Scaffold"),
       ),
       body: new Text("This is body"),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: (){
-            var dlg = new AlertDialog(
-            title: new Text("Greeting"),
-            content: new Text("Hello World"),
-          );
-          showDialog(context: context, child: dlg);
-        },
-        child: new Icon(Icons.add)
+      drawer: new Container(
+        key: new Key("drawer"),
+        decoration: new BoxDecoration(color: Colors.redAccent),
+        width: 300.0,
+        child: new Text("Drawer"),
       ),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: () {
+            var dlg = new AlertDialog(
+              title: new Text("Greeting"),
+              content: new Text("Hello World"),
+            );
+            showDialog(context: context, child: dlg);
+          },
+          child: new Icon(Icons.add)),
     );
   }
 }
