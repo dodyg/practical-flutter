d092523ea7dd6a1bf48e6bebac4ac46484fd26ca
Dody Gunawinata <dodyg@silverkeytech.com>
2018-03-01 16:43:00 +0200

    The form looks OK now

--- projects/ui/lib/pages/form_page.dart
+++ projects/ui/lib/pages/form_page.dart
@@ -13,8 +13,35 @@ class _FormPageState extends State<FormPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(),
-      body: new Container(child: new Form(key: _formKey, child: null)),
-    );
+        appBar: new AppBar(),
+        body: new Container(
+            padding: new EdgeInsets.all(20.0),
+            child: new Form(
+                key: _formKey,
+                child: new ListView(
+                  children: <Widget>[
+                    new TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: new InputDecoration(labelText: "Name"),
+                    ),
+                    new TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: new InputDecoration(labelText: "Age"),
+                    ),
+                    new TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: new InputDecoration(labelText: "Email"),
+                    ),
+                    new TextFormField(
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        decoration: new InputDecoration(labelText: "Password")),
+                    new Container(
+                        child: new RaisedButton(
+                      child: new Text("Register"),
+                      onPressed: null,
+                    ))
+                  ],
+                ))));
   }
 }
