2bda1dcfde40e8020a4a7c1fc4a5a6f29528c2f9
Dody Gunawinata <dodyg@silverkeytech.com>
2018-03-06 19:48:21 +0200

    Dart 2!

--- projects/ui/lib/apps/rss_app.dart
+++ projects/ui/lib/apps/rss_app.dart
@@ -6,20 +6,18 @@ import 'dart:convert';
 
 class _RssAppState extends State<RssApp> {
   String _output = "Waiting";
-  
+
   Future increment() async {
     var client = new HttpClient();
     var req = await client.get("www.scripting.com", 80, "rss.xml");
     var res = await req.close();
-    
+
     var completer = new Completer();
     var contents = new StringBuffer();
-    res.transform(UTF8.decoder).listen((String data) 
-    {
+    res.transform(UTF8.decoder).listen((String data) {
       contents.write(data);
-    }, 
-    onDone: () => completer.complete(contents.toString()));
-    
+    }, onDone: () => completer.complete(contents.toString()));
+
     String rss = await completer.future;
 
     var parsed = parse(rss);
@@ -31,7 +29,7 @@ class _RssAppState extends State<RssApp> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
+      appBar: AppBar(
         title: new Text("RSS"),
       ),
       body: new Column(
